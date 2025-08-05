0c7a9c5bfa5d4109cfebf4888929d3fc045e7f88
Tuhin <www.mdtuhin4556@gmail.com>
2025-08-05 19:30:18 +0600

    Refactor media and static file handling in settings, URLs, and Nginx configuration

--- blog/settings.py
+++ blog/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 # fro Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT =os.path.join(BASE_DIR ,'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL  = 'accounts.User'
--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('home.urls')),
     path('accounts/', include('accounts.urls'))
-]+static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+]
 
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
